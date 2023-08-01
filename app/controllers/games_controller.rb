5eaebd97355446fbb8a1e77397f97a98b6f4efdc
gabriel leibel <gfleibel@hotmail.com>
2023-08-01 16:07:28 -0300

    styled game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,16 +4,20 @@ class GamesController < ApplicationController
   def new
     @letters = ('A'..'Z').to_a.shuffle.sample(10)
     @score = session[:score]
+    @counter = session[:counter]
   end
 
   def score
     @word = params[:word].downcase.split("")
     @score = 0
+    session[:counter] = 3 unless session[:counter]
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     @response = JSON.parse(URI.open(url).read)
     @letters = params[:letters].downcase.split
     if (@word & @letters) != @word
       @message = "Sorry, but #{params[:word]} can't be  built out of #{@letters.join(", ").upcase}."
+      session[:counter] -= 1
+      @score = session[:score]
     elsif (@word & @letters) == @word && @response["found"] == true
       @message = "The word #{params[:word]} is valid according to the grid and is an English word."
       @score += @word.size
@@ -24,6 +28,18 @@ class GamesController < ApplicationController
       end
     else
       @message = "The word #{params[:word]} is valid according to the grid, but is not a valid English word."
+      session[:counter] -= 1
+      @score = session[:score]
     end
+    if session[:counter].zero? || session[:counter].negative?
+      @message = "You have no more tries left. The game will now reset."
+      @score = session[:score]
+    end
+  end
+
+  def reset
+    session[:score] = 0
+    session[:counter] = 3
+    redirect_to new_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "games#new"
   get "new", to: "games#new"
   post "score", to: "games#score"
+  post '/reset_score', to: 'games#reset', as: 'reset_score'
+
 end
